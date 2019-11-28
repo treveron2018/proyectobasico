eadf35bea36620d4c0708d0bc70bc72edec485b0
Andres <trevvieaamb@gmail.com>
2019-11-28 12:39:42 +0100

    logicos

--- basicosAndres/src/basicosAndres/basicosAndres.java
+++ basicosAndres/src/basicosAndres/basicosAndres.java
@@ -2,4 +2,23 @@ package basicosAndres;
 
 public class basicosAndres {
 
+	int a=3, b=8;
+	
+	public void logicos() {
+		
+		if (a>b) {
+			
+			System.out.println("a es mayor que b");
+			
+		} else if (a<b) {
+			System.out.println("b es mayor que a");
+			
+		} else if (a==b) {
+			System.out.println("a y b son iguales");
+		}
+	}
+		
+		
 }
+	
+
